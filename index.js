02dec4ad9f4e79c387318c2d026f98746f041d1b
Ntsoccer <2460socceronly0512@gmail.com>
2021-01-19 00:24:15 +0900

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const list = ['掃除', '買い物', '散歩'];
-
+console.log(`現在持っているタスク一覧\n`)
 for (let i = 0; i < list.length; i++) {
   console.log(`${i}:${list[i]}`);
 }
 
 function list_add() {
+  console.log('現在持っているタスク一覧\n')
   for (let i = 0; i < list.length; i++) {
     console.log(`${i}:${list[i]}`);
   }
   i = 2;
   while (i < list.length) {
     i++;
-    text = prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");
-    list.push(text);
+    const task = prompt('タスクを登録してください')
+    list.push(task);
     alert("新しいタスクを追加しました");
     console.log(`${i}:${list[i]}`);
+    text = prompt("「確認,追加,削除,終了」の4つのいずれかを入力してください");
   }
 }
 
